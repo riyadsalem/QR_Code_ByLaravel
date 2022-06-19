92bec34858a5d847b812909b4d2cf2aa4ad5cc36
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-06-19 16:04:19 +0300

    Edit >> qr_builder , QrController and Create public.qr-code

--- app/Http/Controllers/QrController.php
+++ app/Http/Controllers/QrController.php
@@ -3,19 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Illuminate\Support\Facades\Validator;
-
-
+use Illuminate\Support\Str;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use Spatie\Color\Hex;
 
 class QrController extends Controller
 {
     
-    public function index(){
+  public function index(){
         return view('qr_code.qr_builder');
-    } // End Method
+  } // End Method
 
-    public function do_qr_builder(Request $request){
+  public function do_qr_builder(Request $request){
      //   dd($request->all());
 
      /*
@@ -35,17 +35,30 @@ class QrController extends Controller
    //  return $request->all();
 
    $name = $request->input('name');
-   $body = $request->body;
+   $qr_img_type = $request->input('qr_img_type') ?? 'png';
+   $code = Str::slug($name) . '.' . $qr_img_type; // Riyad Salem >> riyad-salem
+   $body = $request->input('body');
+   $qr_size = $request->input('qr_size') ?? '300';
+   $qr_correction = $request->input('qr_correction') ?? 'H';
+   $qr_encoding = $request->input('qr_encoding') ?? 'UTF-8';
+
+
+      
    
-   $code = QrCode::generate($body);
+
+   $qr = QrCode::format($qr_img_type);
+   $qr->size($qr_size);
+   $qr->errorCorrection($qr_correction);
+   $qr->encoding($qr_encoding);
+   $qr->generate($body, '../public/qr_code/' . $code);
 
    return back()->with([
-    'status' => 'QR Code Generated Successfully',
-    'code' => $code,
+       'status' => 'QR Code generated successfully!',
+       'code' => $code
    ]);
 
 
-    } // End Method
+  } // End Method
 
 }
 
--- resources/views/qr_code/qr_builder.blade.php
+++ resources/views/qr_code/qr_builder.blade.php
@@ -34,6 +34,94 @@
                                     @error('body')<span class="text-danger">{{ $message }}</span>@enderror
                                 </div>
 
+                                <hr>
+
+                                <div class="row">
+                                    <div class="col-3">
+
+                                       <div class="mb-3">
+                                          <label for="qr_size" class="form-label" >QR Size</label>
+                                          <select name="qr_size" class="form-control">
+                                            <option value="">Select Size</option>
+                                            <option value="300">300*300</option>
+                                            <option value="600">600*600</option>
+                                            <option value="900">900*900</option>
+                                          </select> <!-- End: select -->
+                                       </div>
+
+                                    </div> <!-- End: col-3 -->
+
+                                    <div class="col-3">
+
+                                       <div class="mb-3">
+                                          <label for="qr_img_type" class="form-label" >Image Type</label>
+                                          <select name="qr_img_type" class="form-control">
+                                            <option value="">Select Image Type</option>
+                                            <option value="png">PNG</option>
+                                            <option value="svg">SVG</option>
+                                            <option value="eps">EPS</option>
+                                          </select> <!-- End: select -->
+                                       </div>
+
+                                    </div> <!-- End: col-3 -->
+                                    
+
+                                    <div class="col-3">
+
+                                       <div class="mb-3">
+                                          <label for="qr_correction" class="form-label" >QR Correction</label>
+                                          <select name="qr_correction" class="form-control">
+                                            <option value="">Select QR Correction</option>
+                                            <option value="L">7%</option>
+                                            <option value="M">15%</option>
+                                            <option value="Q">25%</option>
+                                            <option value="H">30%</option>
+                                          </select> <!-- End: select -->
+                                       </div>
+
+                                    </div> <!-- End: col-3 -->
+                                    
+                                    
+                                    <div class="col-3">
+
+                                       <div class="mb-3">
+                                          <label for="qr_encoding" class="form-label" >QR Encoding</label>
+                                          <select name="qr_encoding" class="form-control">
+                                            <option value="">Select QR Encoding</option>
+                                            <option value="UTF-8">UTF-8</option>
+                                            <option value="ISO-8859-1">ISO-8859-1</option>
+                                            <option value="ISO-8859-2">ISO-8859-2</option>
+                                            <option value="ISO-8859-3">ISO-8859-3</option>
+                                            <option value="ISO-8859-4">ISO-8859-4</option>
+                                            <option value="ISO-8859-5">ISO-8859-5</option>
+                                            <option value="ISO-8859-6">ISO-8859-6</option>
+                                            <option value="ISO-8859-7">ISO-8859-7</option>
+                                            <option value="ISO-8859-8">ISO-8859-8</option>
+                                            <option value="ISO-8859-9">ISO-8859-9</option>
+                                            <option value="ISO-8859-10">ISO-8859-10</option>
+                                            <option value="ISO-8859-11">ISO-8859-11</option>
+                                            <option value="ISO-8859-12">ISO-8859-12</option>
+                                            <option value="ISO-8859-13">ISO-8859-13</option>
+                                            <option value="ISO-8859-14">ISO-8859-14</option>
+                                            <option value="ISO-8859-15">ISO-8859-15</option>
+                                            <option value="ISO-8859-16">ISO-8859-16</option>
+                                            <option value="SHIFT-JIS">SHIFT-JIS</option>
+                                            <option value="WINDOWS-1250">WINDOWS-1250</option>
+                                            <option value="WINDOWS-1251">WINDOWS-1251</option>
+                                            <option value="WINDOWS-1252">WINDOWS-1252</option>
+                                            <option value="WINDOWS-1256">WINDOWS-1256</option>
+                                            <option value="UTF-16BE">UTF-16BE</option>
+                                            <option value="ASCII">ASCII</option>
+                                            <option value="GBK">GBK</option>
+                                            <option value="EUC-KR">EUC-KR</option>
+                                          </select> <!-- End: select -->
+                                       </div>
+
+                                    </div> <!-- End: col-3 -->
+                                
+                                
+                                </div><!-- End: row -->
+
                                 <div class="mb-3">
                                     <button type="submit" name="submit" class="btn btn-primary"> Generate QR Code </button>
                                 </div>
@@ -45,7 +133,7 @@
 
                         <div class="col-4">
                          @if (session('status'))
-                         {!! session('code') !!}
+                         <img src="{{ asset('qr_code/' . session('code')) }}" alt="{{ session('code') }}" class="img-fluid">
                          @endif
 
                         </div> <!-- End: col-4 ImageQrCode -->
